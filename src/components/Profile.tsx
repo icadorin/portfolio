b31d3baed7c5bbaa415974b29573fed1445835b5
origin <cadorinisrael@gmail.com>
2025-03-26 21:13:22 -0300

    Enhancements CSS and development projects section

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../profile.css";
 import { GitHub, LinkedIn } from '@mui/icons-material';
 import Folder from "@mui/icons-material/FolderOutlined";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import companies from "../data/companiesData";
 
 const Profile: React.FC = () => {
+  const [selectedCompany, setSelectedCompany] = useState(companies[0]);
+
   return (
     <section className="profile-container">
       <header className="header">
         <nav className="nav-links">
           <button onClick={() => document.getElementById("sobre")?.scrollIntoView({ behavior: "smooth" })}>
-            Sobre
+            Sobre Mim
           </button>
           <button onClick={() => document.getElementById("experiencia")?.scrollIntoView({ behavior: "smooth" })}>
             Experiência
           </button>
-          <button onClick={() => document.getElementById("trabalhos")?.scrollIntoView({ behavior: "smooth" })}>
-            Trabalhos
+          <button onClick={() => document.getElementById("Projetos")?.scrollIntoView({ behavior: "smooth" })}>
+            Projetos
           </button>
           <button onClick={() => document.getElementById("contatos")?.scrollIntoView({ behavior: "smooth" })}>
             Contatos
@@ -45,8 +49,8 @@ const Profile: React.FC = () => {
         <section id="sobre" className="about">
           <div className="profile-about">
             <h2 className="section-about">Sobre Mim</h2>
-            <div className="profile-text">
-              <p className="profile-description">
+            <div className="profile-description">
+              <p>
                 Sou desenvolvedor full-stack, utilizo diversas tecnologias e gosto
                 de aplicar minhas habilidades para solucionar problemas.
                 Trabalhar em equipe é essencial para criar soluções inovadoras e eficientes.
@@ -54,8 +58,8 @@ const Profile: React.FC = () => {
             </div>
           </div>
           <div className="photo-container">
-            <div className="photo-border">
-              <div className="profile-photo"></div>
+            <div className="profile-photo">
+              <div className="photo-border"></div>
             </div>
           </div>
         </section>
@@ -63,11 +67,40 @@ const Profile: React.FC = () => {
         <section id="experiencia" className="experience">
           <h2 className="section-experience">Experiência</h2>
           <div className="experience-container">
+            <div className="exp-contents-container">
+              {companies.map((company, index) => (
+                <button
+                  key={index}
+                  className={`exp-button ${selectedCompany.name === company.name ? "selected" : ""}`}
+                  onClick={() => setSelectedCompany(company)}
+                >
+                  {company.name}
+                </button>
+              ))}
+            </div>
 
+            <div className="exp-description">
+              <div className="exp-desc-container">
+                <p className="position">{selectedCompany.position}</p>
+                <p className="company-name">{selectedCompany.name}</p>
+              </div>
+              <p className="time-worked">{selectedCompany.period}</p>
+              <div>
+                {[selectedCompany.descriptionP1, selectedCompany.descriptionP2].map(
+                  (desc, index) =>
+                    desc && (
+                      <div key={index} className="task-item">
+                        <ArrowRightIcon />
+                        <p>{desc}</p>
+                      </div>
+                    )
+                )}
+              </div>
+            </div>
           </div>
         </section>
 
-        <section id="trabalhos" className="works">
+        <section id="projetos" className="works">
           <h2 className="section-works">Projetos</h2>
           <p className="work-description">
             Sistema de gerenciamento de tarefas, plataforma e-commerce e aplicativo mobile.
@@ -81,7 +114,7 @@ const Profile: React.FC = () => {
                   </div>
                   <a
                     className="github-icon"
-                    href="https://github.com/user"
+                    href="https://github.com/username!#!@$!$!"
                     target="_blank" rel="noopener noreferrer"
                   >
                     <GitHub sx={{ fontSize: 25, color: " #1d0327" }} />
@@ -105,14 +138,14 @@ const Profile: React.FC = () => {
           <h2 className="section-contact">Contato</h2>
           <div className="link-format">
             <a
-              href="https://www.linkedin.com/in/user"
+              href="https://www.linkedin.com/in/username!#!@$!$!"
               target="_blank"
               rel="noopener noreferrer"
             >
               <LinkedIn sx={{ color: 'white' }} />
             </a>
             <a
-              href="https://github.com/user"
+              href="https://github.com/username!#!@$!$!"
               target="_blank" rel="noopener noreferrer"
             >
               <GitHub sx={{ color: 'white' }} />
